66597881978cf5eba0f5f9f29d2707bb5d1be059
Muhammad Awab <muhammadawab82002@gmail.com>
2023-07-22 15:45:12 +0500

    a

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,7 @@
 # Assuming your Node.js server is running on a specific port (e.g., 3000)
 # You can customize the port number and the command based on your application
 
-echo "Stopping Node.js server..."
+echo "Stopping Node.js server...."
 
 # Find the process ID (PID) of the running Node.js server
 PID=$(lsof -t -i :3000)
